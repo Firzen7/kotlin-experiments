09b9b54e521b696ca726ec0b641434a85485e7fc
ondrej <ondrej@sanctusmedia.com>
2020-11-23 18:17:02 +0100

    Chapter 4 small update

--- src/net/firzen/kotlinexperiments/Chapter4.kt
+++ src/net/firzen/kotlinexperiments/Chapter4.kt
@@ -17,6 +17,67 @@ fun main() {
     Animal.Chicken().identify()
 
     Cat().identify()
+
+    userTest()
+
+    dataClassTest()
+
+    byTest()
+}
+
+/**
+ * By keyword is used to create delegates automatically without too much of boilerplate code.
+ * Normally, the Decorator pattern is used for implementing delegates.
+ */
+private fun byTest() {
+    val decorated = DecoratedCollection<Int>()
+    decorated.add(5)
+    decorated.add(7)
+
+    decorated.forEach { println(it) }
+
+    val delegated = DelegatedCollection<Int>()
+    delegated.add(5)
+    delegated.add(7)
+
+    delegated.forEach { println(it) }
+}
+
+class DelegatedCollection<T> (private val innerList: MutableCollection<T> = arrayListOf())
+    : Collection<T> by innerList {
+
+    fun add(element: T) = innerList.add(element)
+}
+
+/**
+ * This is a child of Collection implemented by traditional Decorator pattern.
+ * Notice the amount of boilerplate code.
+ */
+class DecoratedCollection<T> : MutableCollection<T> {
+    private val innerList = arrayListOf<T>()
+
+    override fun add(element: T) = innerList.add(element)
+    override fun addAll(elements: Collection<T>): Boolean = innerList.addAll(elements)
+    override val size: Int get() = innerList.size
+    override fun contains(element: T): Boolean = innerList.contains(element)
+    override fun containsAll(elements: Collection<T>): Boolean = innerList.containsAll(elements)
+    override fun isEmpty(): Boolean = innerList.isEmpty()
+    override fun iterator(): MutableIterator<T> = innerList.iterator()
+    override fun clear() = innerList.clear()
+    override fun remove(element: T): Boolean = innerList.remove(element)
+    override fun removeAll(elements: Collection<T>): Boolean = removeAll(elements)
+    override fun retainAll(elements: Collection<T>): Boolean = retainAll(elements)
+}
+
+private fun dataClassTest() {
+    val alan = BestClass("Alan", 19)
+    println("Data classes have pre-generated methods equals, hashCode and toString!")
+    println("${alan.toString()} --> ${alan.hashCode()}")
+
+    // Also, data classes contain pre-generated copy function!!
+    val john = alan.copy(name = "John")
+    println("${john.name} was born, but ${alan.name} remained unchanged!")
+    println("${john.name}'s hash is ${john.hashCode()}, and ${alan.name}'s is ${alan.hashCode()}")
 }
 
 interface Clickable {
@@ -88,6 +149,57 @@ sealed class Animal(private val legs: Int) {
     class Snake : Animal(0)
 }
 
-// This is still possible,as long as we are in the same file.
+// This is still possible, as long as we are in the same file.
 // It wouldn't work outside of this file.
 class Cat : Animal(4)
+
+// Kotlin classes more in depth:
+
+fun userTest() {
+    val user = User("Mike")
+    println(user.nickname)
+
+    val user1 = User1("Fred")
+    println(user1.nickname)
+
+    val user3 = User3("LilHusky")
+//    println(user3.nickname)    INVALID CODE!
+
+    val specialUser = SpecialUser("Jetty")
+    println("Age: ${specialUser.age}, Nick: ${specialUser.nickname}")
+
+    val ee = Empty()
+}
+
+class User(_nickname: String) {     // <-- primary constructor
+    val nickname: String            // field
+
+    // primary initializer
+    // (to be used together with primary constructors)
+    init {
+        nickname = _nickname
+    }
+}
+
+// this class is equivalent to class User
+class User1(_nickname: String) {
+    val nickname = _nickname
+}
+
+// equivalent of User and User1
+class User2(val nickname: String)
+
+// nickname won't be accessible with this declaration
+class User3(nickname: String)
+
+
+// primary constructor with default parameters
+open class NormalUser(val age: Int, val nickname: String = "No nickname")
+
+// println("Age: ${specialUser.age}, Nick: ${specialUser.nickname}") <-- works normally
+class SpecialUser(nickname: String) : NormalUser(21, nickname)
+
+// this class still has a default constructor
+class Empty
+
+data class BestClass(val name: String, val age: Int)
